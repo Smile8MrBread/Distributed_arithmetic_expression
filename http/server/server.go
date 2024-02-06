120e6bb1bf0a745e17078eb2636c3b6924cf0863
Vlad Vlasov <vlsv.3vl@gmail.com>
2024-02-06 17:55:56 +0300

    Application sceleton v0.12

--- http/server/server.go
+++ http/server/server.go
@@ -5,7 +5,7 @@ import (
 	"github.com/Smile8MrBread/Distributed_arithmetic_expression/http/server/handler"
 )
 
-func StartServer(port string) {
+func StartServer() {
 	server := http.Server{
 		Addr: "127.0.0.1:8123",
 	}
